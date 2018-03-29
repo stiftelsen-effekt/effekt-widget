062db01e0149f547affef5fb62c504c8ade8f2b6
Håkon Harnes <sosser@skymiastudios.com>
2018-03-29 13:26:13 +0200

    Dunno

--- script/helpers/network.js
+++ script/helpers/network.js
@@ -1,10 +1,18 @@
 var api_url = "https://api.gieffektivt.no/";
+<<<<<<< HEAD
 //var api_url = "http://localhost:3000/"; 
+=======
+//var api_url = "http://localhost:3000/";
+>>>>>>> f5e5ba07a85f161795e87a86e42ea13473630b59
 
 module.exports = {
     request: function(endpoint, type, data, cb) {
         var http = new XMLHttpRequest();
         var url = api_url + endpoint;
+<<<<<<< HEAD
+=======
+
+>>>>>>> f5e5ba07a85f161795e87a86e42ea13473630b59
 
         http.onreadystatechange = function() {
             if (this.readyState == 4) {
