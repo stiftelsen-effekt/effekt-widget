d3fe913e35629a8a6a49bb2de24c547e2ca74c9c
Håkon Harnes <haakoeha@stud.ntnu.no>
2019-07-07 18:14:34 +0200

    Fixes HTML encoding issue

--- gulpfile.js
+++ gulpfile.js
@@ -10,35 +10,26 @@ const buffer = require('vinyl-buffer');
 const log = require('gulplog');
 const sourcemaps = require('gulp-sourcemaps');
 const assign = require('lodash.assign');
+var replace = require('gulp-replace');
 
-// add custom browserify options here
 const customOpts = {
   entries: ['./script/widget.js'],
   debug: true
 };
 var opts = assign({}, watchify.args, customOpts);
-//var b = watchify(browserify(opts));
 var b = browserify(customOpts);
 
-// add transformations here
-// i.e. b.transform(coffeeify);
-
-b.on('update', bundle); // on any dep update, runs the bundler
-b.on('log', log.info); // output build logs to terminal
+b.on('update', bundle);
+b.on('log', log.info);
 
 function bundle() {
   return b.bundle()
-    // log errors if they happen
     .on('error', log.error.bind(log, 'Browserify Error'))
     .pipe(source('bundle.js'))
-    
-    // optional, remove if you don't need to buffer file contents
     .pipe(buffer())
     .pipe(minify())
-    // optional, remove if you dont want sourcemaps
-    .pipe(sourcemaps.init({loadMaps: true})) // loads map from browserify file
-       // Add transformation tasks to the pipeline here.
-    .pipe(sourcemaps.write('./')) // writes .map file
+    .pipe(sourcemaps.init({loadMaps: true}))
+    .pipe(sourcemaps.write('./'))
     .pipe(gulp.dest('./dist'));
 }
 
@@ -50,6 +41,17 @@ function styles() {
 
 function html() {
   return gulp.src('./widget.htm')
+    /*  Google Cloud Storage does not play nice with UTF-8 encoding.
+        Replacing the characters with HTML entities fixes the problem,
+        although it is of course a dirty fix. They way it is done here
+        is terribly inefficent as we loop over the entire file six times,
+        however since it is a build step, performance is not very important. */
+    .pipe(replace('Æ', '&Aelig;'))
+    .pipe(replace('æ', '&aelig;'))
+    .pipe(replace('Ø', '&Oslash;'))
+    .pipe(replace('ø', '&oslash;'))
+    .pipe(replace('Å', '&Aring;'))
+    .pipe(replace('å', '&aring;'))
     .pipe(gulp.dest('./dist'))
 }
 
