7c8a35befd31bce1f309274ad8be04828c46e817
Håkon Harnes <hh@eiendomsverdi.no>
2020-04-16 15:42:06 +0200

    Updates network to use cloud run instance

--- script/helpers/network.js
+++ script/helpers/network.js
@@ -1,6 +1,5 @@
 module.exports = {
-    api_url: "https://api.gieffektivt.no/",
-    //api_url: "http://localhost:3000/",
+    api_url: "https://data.gieffektivt.no/",
 
     request: function(endpoint, type, data, cb) {
         var http = new XMLHttpRequest();
