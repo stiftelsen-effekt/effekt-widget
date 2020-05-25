e965cc951e7080bfb502ac8792a9472c9a052222
Håkon Harnes <hh@eiendomsverdi.no>
2020-05-25 12:45:28 +0200

    Updates node environment

--- ci-test.sh
+++ ci-test.sh
@@ -1,6 +1,5 @@
 apt-get -q update &&
 apt-get -qqy install xvfb libgtk2.0-0 libnotify-dev libgconf-2-4 libnss3 libxss1 libasound2 nodejs npm &&
-export NODE_ENV=dev &&
 npm install &&
 npm install -g gulp &&
 npm run build &&
