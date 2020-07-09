39c38aa6d68e67231176ae1649a65197216fac63
Håkon Harnes <haakoeha@stud.ntnu.no>
2020-07-09 14:22:43 +0200

    Fixes cypress tests scrolling in widget

--- cypress/integration/widget.spec.js
+++ cypress/integration/widget.spec.js
@@ -6,20 +6,20 @@ context('Actions', () => {
     })
 
     it('A standard bank donation runs through', () => {
-        cy.get('#donation-btn').click()
+        cy.get('#donation-btn').click({force: true})
 
         cy.get("#donation-widget-container").should('have.class', 'active')
 
-        cy.get('#donation-widget .pane.payment-method .payment-methods .method.bank').click()
+        cy.get('#donation-widget .pane.payment-method .payment-methods .method.bank').click({force: true})
 
         onPaneOffset(1)
 
         let random = Math.random().toString(36).substring(7)
         let randommail = random + '@testeffekt.com'
-        cy.get(`${pane('basic')} .name`).type(random)
-        cy.get(`${pane('basic')} .email`).type(randommail)
+        cy.get(`${pane('basic')} .name`).type(random, {force: true})
+        cy.get(`${pane('basic')} .email`).type(randommail, {force: true})
 
-        cy.get(`${pane('basic')} #check-privacy-policy`).not('[disabled]').check().should('be.checked')
+        cy.get(`${pane('basic')} #check-privacy-policy`).not('[disabled]').check({force: true}).should('be.checked')
         submitPane('basic')
 
         onPaneOffset(2)
@@ -28,7 +28,7 @@ context('Actions', () => {
         submitPane('amount')
         onPaneOffset(3)
 
-        cy.get(`${pane('referral')} #referral-list li`).first().click()
+        cy.get(`${pane('referral')} #referral-list li`).first().click({force: true})
         //Referrals are simply hidden when submitting, so offset should remain 3
         onPaneOffset(3)
 
@@ -42,7 +42,7 @@ const pane = (paneName) => {
 }
 
 const submitPane = (paneName) => {
-    cy.get(`${pane(paneName)} .btn.frwd`).click()
+    cy.get(`${pane(paneName)} .btn.frwd`).click({force: true})
 }
 
 const onPaneOffset = (offsetNumber) => {
