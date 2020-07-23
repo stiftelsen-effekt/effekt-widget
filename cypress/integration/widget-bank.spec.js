3356f7f86a642351d4c539658c31c0c776ff063f
Philip Hall <philip.h.andersen@gmail.com>
2020-07-23 16:53:02 +0200

    Added Cypress tests for widget

--- cypress/integration/widget-bank.spec.js
+++ cypress/integration/widget-bank.spec.js
@@ -1,50 +1,36 @@
-/// <reference types="cypress" />
-
 context('Actions', () => {
     beforeEach(() => {
         cy.visit('./widget-host-local.htm')
     })
 
-    it('Goes through the mutual functionality between all donation methods', () => {
+    it('A standard bank donation runs through', () => {
         cy.get('#donation-btn').click({force: true})
+
         cy.get("#donation-widget-container").should('have.class', 'active')
+
         cy.get('#donation-widget .pane.payment-method .payment-methods .method.bank').click({force: true})
-        
+
         onPaneOffset(1)
 
         let random = Math.random().toString(36).substring(7)
         let randommail = random + '@testeffekt.com'
         cy.get(`${pane('basic')} .name`).type(random, {force: true})
         cy.get(`${pane('basic')} .email`).type(randommail, {force: true})
-        cy.get(`${pane('basic')} #check-tax-deduction`).not('[disabled]').check({force: true}).should('be.checked')
-        cy.get(`${pane('basic')} #check-privacy-policy`).click()
-        cy.get(`${pane('basic')} .ssn`).type("123456789")
 
+        cy.get(`${pane('basic')} #check-privacy-policy`).not('[disabled]').check({force: true}).should('be.checked')
         submitPane('basic')
-        onPaneOffset(2)
-        goBack('basic')
-        onPaneOffset(1)
-        submitPane('basic')
+
         onPaneOffset(2)
 
-        cy.get(`${pane('amount')} #check-select-single`).click({force: true})
-        cy.get(`${pane('amount')} #check-select-split`).click({force: true})
-        submitPane('amount')
-        onPaneOffset(3)
-        goBack('amount')
-        onPaneOffset(2)
+        cy.get(`${pane('amount')} #check-select-recommended`).not('[disabled]').should('be.checked')
         submitPane('amount')
         onPaneOffset(3)
 
-        submitPane('shares')
-        //cy.get(`${pane('referral')} #referral-list li`).first().click({force: true})
-
-        //TODO: Check that shares work properly
-        //TODO: Check that all backwards buttons work properly
-
-        //cy.get(`${pane('referral')} #referral-list li`).first().click({force: true})
+        cy.get(`${pane('referral')} #referral-list li`).first().click({force: true})
         //Referrals are simply hidden when submitting, so offset should remain 3
-        //onPaneOffset(3)
+        onPaneOffset(3)
+
+        cy.get(`${pane('result')} .email:first-child`).first().should('have.text', randommail)
     })
 })
 
@@ -55,10 +41,6 @@ const pane = (paneName) => {
 
 const submitPane = (paneName) => {
     cy.get(`${pane(paneName)} .btn.frwd`).click({force: true})
-}
-
-const goBack = (paneName) => {
-    cy.get(`${pane(paneName)} .btn.back`).click({force: true})
 }
 
 const onPaneOffset = (offsetNumber) => {
---
+++ cypress/integration/widget-paypal.spec.js
@@ -0,0 +1,52 @@
+context('Actions', () => {
+    beforeEach(() => {
+        cy.visit('./widget-host-local.htm')
+    })
+
+    it('A standard paypal donation runs through', () => {
+        cy.get('#donation-btn').click({force: true})
+
+        cy.get("#donation-widget-container").should('have.class', 'active')
+
+        cy.get('#donation-widget .pane.payment-method .payment-methods .method.paypal').click({force: true})
+
+        onPaneOffset(1)
+
+        let random = Math.random().toString(36).substring(7)
+        let randommail = random + '@testeffekt.com'
+        cy.get(`${pane('basic')} .name`).type(random, {force: true})
+        cy.get(`${pane('basic')} .email`).type(randommail, {force: true})
+
+        cy.get(`${pane('basic')} #check-privacy-policy`).not('[disabled]').check({force: true}).should('be.checked')
+        submitPane('basic')
+        onPaneOffset(2)
+
+        let ranAmount = Math.floor(Math.random() * 1000) + 100
+        cy.get(`${pane('amount')} .amount-wrapper .amount`).type(ranAmount)
+        cy.get(`${pane('amount')} #check-select-recommended`).not('[disabled]').should('be.checked')
+
+        submitPane('amount')
+        //TODO: assert that widget.donationAmount is equal to ranAmount 
+        onPaneOffset(3)
+
+        cy.get(`${pane('referral')} #referral-list li`).first().click({force: true})
+        // Referrals are simply hidden when submitting, so offset should remain 3
+        onPaneOffset(3)
+
+        cy.get(`${pane('paypal')} #widget-paypal-button`).click({force: true})
+    })
+})
+
+/* Helper functions */
+const pane = (paneName) => {
+    return `#donation-widget .pane.${paneName}`
+}
+
+const submitPane = (paneName) => {
+    cy.get(`${pane(paneName)} .btn.frwd`).click({force: true})
+}
+
+const onPaneOffset = (offsetNumber) => {
+    //Cypress reads the computed matrix value instead of translateX(-320px)
+    cy.get('#donation-widget .slider').should('have.css', 'transform', `matrix(1, 0, 0, 1, ${-320*offsetNumber}, 0)`)
+}
