9e1caa1f03faa2e611c43c2b7c0b5c1f88356b49
Philip Hall <philip.h.andersen@gmail.com>
2020-08-03 00:14:47 +0200

    Added tests for shares UI

--- cypress/integration/widget.spec.js
+++ cypress/integration/widget.spec.js
@@ -6,9 +6,12 @@ context('Actions', () => {
     })
 
     it('Goes through mutual functionality between all donation methods', () => {
-        cy.get('#donation-btn').click({force: true})
+
+        //TODO: Replace all classes and id's with cy-data
+
+        cy.get('#donation-btn').click({force: true}) //Hvorfor er denne knappen forskjellig i local og prod?
         cy.get("#donation-widget-container").should('have.class', 'active')
-        cy.get('#donation-widget .pane.payment-method .payment-methods .method.bank').click({force: true})
+        cy.get('[data-cy=method-bank]').click({force: true})
         cy.onPaneOffset(1)
 
         let random = Math.random().toString(36).substring(7)
@@ -25,13 +28,44 @@ context('Actions', () => {
         cy.getInPane('amount', '#check-select-split').click({force: true})
         cy.nextPane('amount')
         cy.onPaneOffset(3)
+
         cy.server()
+        cy.request('GET', 'https://data.gieffektivt.no/organizations/active').then((response) => {
+            let orgs = response.body.content
+
+            //TODO: Assert sum of shares not higher or lower than 100
+            cy.get(`[data-cy=${orgs[0].abbriv}-share]`).type('{backspace}{backspace}{backspace}')
+            cy.get(`[data-cy=${orgs[0].abbriv}-share]`).type('101')
+            cy.get('[data-cy=btn-frwd]').should('have.class', 'inactive')
+            cy.get('[data-cy=total]').should('not.have.class', 'total-hidden')
+
+            cy.get(`[data-cy=${orgs[0].abbriv}-share]`).type('{backspace}{backspace}{backspace}')
+            cy.get(`[data-cy=${orgs[0].abbriv}-share]`).type('99')
+            cy.get('[data-cy=btn-frwd]').should('have.class', 'inactive')
+            cy.get('[data-cy=total]').should('not.have.class', 'total-hidden')
+
+            cy.get(`[data-cy=${orgs[0].abbriv}-share]`).type('{backspace}{backspace}{backspace}')
+            cy.get(`[data-cy=${orgs[0].abbriv}-share]`).type('100')
+            cy.get('[data-cy=btn-frwd]').should('not.have.class', 'inactive')
+            cy.get('[data-cy=total]').should('have.class', 'total-hidden')
+
+
+            cy.get(`[data-cy=${orgs[0].abbriv}-share]`).type('{backspace}{backspace}{backspace}')
+            //Dynamically fills in all shares to equal 100 (Only works for less than 10 organizations)
+            for (i = 0; i < orgs.length; i++) {
+                if (i != orgs.length-1) {
+                    cy.get(`[data-cy=${orgs[i].abbriv}-share]`).type('10')
+                }
+                else {
+                    //Gives the remaining percentage to the bottom organization
+                    cy.get(`[data-cy=${orgs[i].abbriv}-share]`).type(100-(orgs.length-1)*10)
+                }
+            }
+        })
+        
         cy.route('POST', '/donations/register*').as('register')
         cy.route('POST', '/donations/bank/pending*').as('pending')
-
         cy.nextPane('shares')
-
-        
         cy.onPaneOffset(4)
 
         cy.prevPane('referral')
@@ -45,14 +79,16 @@ context('Actions', () => {
         cy.nextPane('shares')
         cy.onPaneOffset(4)
 
-         //TODO: Check that shares work properly
-
         //Referrals are simply hidden when submitting, so offset should remain 4
         cy.getInPane('referral', '#referral-list li').first().click({force: true})
 
         cy.wait(['@register', '@pending']).then((xhrs) => {
             console.log(xhrs[0].responseBody)
             console.log(xhrs[1].responseBody)
+
+            //TODO: Assert
+            //xhr.responseCode == 200 NÅR ALT ER RIKTIG
+            //xhr.responseCode == 400 NÅR DET IKKE SUMMER TIL 100, ELLER ER - i tallene, ELLER inneholder ugyldige tegn
         })
         //cy.onPaneOffset(4)
        
